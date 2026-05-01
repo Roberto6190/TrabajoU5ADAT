2cbfb06137e44fc98e7d7b9de314cf83676c2248
tope2 <tope2@host.docker.internal>
2026-05-01 11:29:08 +0200

    cm

---
+++ src/main/java/com/example/demo/TrabajoU52Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoU52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoU52Application.class, args);
+	}
+
+}
